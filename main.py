64b25ae0a66adaf6484209a0761f536718f74563
Alpher <alex.cvua@gmail.com>
2023-05-02 00:23:16 +0300

    00:21: I've done function calc(action, arg1, arg2) action: - + - - - * - / - % - sin - cos
    You should make only the body (choose action and enter args)

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+def calc(action, arg1, arg2=0):
+    match action:
+        case "+":
+            return arg1 + arg2
+        case "-":
+            return arg1 - arg2
+        case "*":
+            return arg1 * arg2
+        case "/":
+            if arg2 != 0:
+                return arg1 / arg2
+            else:
+                return "Division by ZERO"
+        case "%":
+            if arg2 != 0:
+                return arg1 % arg2
+            else:
+                return "Division by ZERO"
+        case "sin":
+            return math.sin(arg1)
+        case "cos":
+            return math.cos(arg1)
 #space for calc
